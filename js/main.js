1cbc77c0747c918ade18c0a6f87718a7a14988d0
wfinigan <wfinigan64@gmail.com>
2019-12-07 13:19:40 -0500

    Fix divs.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 /* main JS file */
 
+// set up animations
+new WOW().init();
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+}
+
 // settings for navigation
 var myFullpage = new fullpage('#fullpage', {
-    anchors: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-    sectionsColor: [],
-    navigation: true,
-    navigationPosition: 'right',
-    navigationTooltips: ['Title', 'Intro', 'Background', 'Map', 'Violations by Location', 'Violations by Hour', 'Violations by Class', 'The Drop Off', 'Policing Strategy', 'Intent to Distribute vs Possession', 'So What?', 'About']
+  anchors: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  sectionsColor: [],
+  navigation: true,
+  navigationPosition: 'right',
+  navigationTooltips: ['Title', 'Intro', 'Background', 'Map', 'Violations by Location', 'Violations by Hour', 'Violations by Class', 'The Drop Off', 'Policing Strategy', 'Intent to Distribute vs Possession', 'So What?', 'About']
 });
-
 
 // Date parser to convert strings to date objects
 var parseDate = d3.timeParse("%Y");
@@ -16,109 +29,104 @@ var formatTime = d3.timeFormat("%Y");
 
 var parseDateYM = d3.timeParse("%m/%Y")
 
-
 var area_chart,
-    stackedAreaChart,
-    setFilter;
+  stackedAreaChart,
+  setFilter;
 
 var smallMults = [];
 
-$('#show-area-chart').on('click', function () {
-    $('#overlay').attr('class', 'overlay-hide')
+$('#show-area-chart').on('click', function() {
+  $('#overlay').attr('class', 'overlay-hide')
 });
 
-$('#show-hint-area').on('click', function () {
-    $('#hint-text').attr("style", 'opacity: 1;')
+$('#show-hint-area').on('click', function() {
+  $('#hint-text').attr("style", 'opacity: 1;')
 })
 
 function stackedAreaMults(data) {
-    // https://appdividend.com/2019/04/11/how-to-get-distinct-values-from-array-in-javascript/
-    const unique = (value, index, self) => {
-        return self.indexOf(value) === index
-    };
+  // https://appdividend.com/2019/04/11/how-to-get-distinct-values-from-array-in-javascript/
+  const unique = (value, index, self) => {
+    return self.indexOf(value) === index
+  };
 
-    var offense_descriptions = ['All Classes'];
+  var offense_descriptions = ['All Classes'];
 
-    data.forEach(function (d) {
-        offense_descriptions.push(d.OFFENSE_DESCRIPTION)
+  data.forEach(function(d) {
+    offense_descriptions.push(d.OFFENSE_DESCRIPTION)
+  });
+
+  var classes = offense_descriptions.filter(unique).sort();
+
+  var dataByClass = {};
+
+  stackedAreaChart = new StackedAreaChart('stacked-area-chart', data);
+
+  classes.forEach(function(d, i) {
+    dataByClass[d] = [];
+    d3.select('#small-mults').append('div')
+      .attr('id', 'small-mult-' + i)
+  });
+
+  // set data for small multiples
+  data.forEach(function(row) {
+    if (dataByClass[row.OFFENSE_DESCRIPTION] != undefined) {
+      dataByClass[row.OFFENSE_DESCRIPTION].push(row)
+    }
+  });
+
+  dataByClass['All Classes'] = data;
+
+  classes.forEach(function(d, i) {
+    smallMults.push(new SmallMult('small-mult-' + i, dataByClass[d], d))
+  });
+
+  d3.select('#all-mults')
+    .append('p')
+    .text('All classes')
+    .on('click', function() {
+      stackedAreaChart.data = data
+      stackedAreaChart.wrangleData()
     });
 
-    var classes = offense_descriptions.filter(unique).sort();
-
-    var dataByClass = {};
-
-    stackedAreaChart = new StackedAreaChart('stacked-area-chart', data);
-
-    classes.forEach(function (d, i) {
-        dataByClass[d] = [];
-        d3.select('#small-mults').append('div')
-            .attr('id', 'small-mult-' + i)
+  // helper function for small multiples
+  setFilter = function setFilter(className) {
+    stackedAreaChart.data = data.filter(function(row) {
+      return (row.OFFENSE_DESCRIPTION == className) || (className == "All Classes")
     });
 
-    // set data for small multiples
-    data.forEach(function(row) {
-        if (dataByClass[row.OFFENSE_DESCRIPTION] != undefined) {
-            dataByClass[row.OFFENSE_DESCRIPTION].push(row)
-        }
-    });
+    stackedAreaChart.wrangleData()
+  };
 
-    dataByClass['All Classes'] = data;
+  var classDData = data.filter(function(row) {
+    return (row.OFFENSE_DESCRIPTION == 'CLASS D')
+  });
 
-    classes.forEach(function (d, i) {
-        smallMults.push(new SmallMult('small-mult-' + i, dataByClass[d], d))
-    });
+  new StackedAreaChart('area-class-d', classDData);
 
-    d3.select('#all-mults')
-        .append('p')
-        .text('All classes')
-        .on('click', function() {
-            stackedAreaChart.data = data
-            stackedAreaChart.wrangleData()
-        });
-
-    // helper function for small multiples
-    setFilter = function setFilter(className) {
-        stackedAreaChart.data = data.filter(function (row) {
-            return (row.OFFENSE_DESCRIPTION == className) || (className == "All Classes")
-        });
-
-        stackedAreaChart.wrangleData()
-    };
-
-    var classDData = data.filter(function (row) {
-        return (row.OFFENSE_DESCRIPTION == 'CLASS D')
-    });
-
-    new StackedAreaChart('area-class-d', classDData);
-
-    return stackedAreaChart
+  return stackedAreaChart
 
 
 };
 
 queue()
-    .defer(d3.json, 'data/Police_Districts.geojson')
-    .defer(d3.csv, "data/drug_data_cats.csv")
-    .await(function(error,dataGeo, dataDrugs) {
-        var areaChart = stackedAreaMults(dataDrugs);
-        new MapPlot('map', dataGeo, dataDrugs, areaChart.colorScale);
-        // (5) Bind event handler
-// when 'selectionChanged' is triggered, specified function is called
+  .defer(d3.json, 'data/Police_Districts.geojson')
+  .defer(d3.csv, "data/drug_data_cats.csv")
+  .await(function(error, dataGeo, dataDrugs) {
+    var areaChart = stackedAreaMults(dataDrugs);
+    new MapPlot('map', dataGeo, dataDrugs, areaChart.colorScale);
+    // (5) Bind event handler
+    // when 'selectionChanged' is triggered, specified function is called
 
 
-    });
-
+  });
 
 d3.csv("data/ClassDViolations.csv", function(data) {
-    console.log(data)
-    linegraph = new LineGraph('linegraph-chart', data)
+  console.log(data)
+  linegraph = new LineGraph('linegraph-chart', data)
 });
 
-$('#show-hint-area').on('click', function () {
-    stackedAreaChart.lineTime.attr("style", 'opacity: 1;')
-    stackedAreaChart.textTime.attr("style", 'opacity: 1;')
+$('#show-hint-area').on('click', function() {
+  stackedAreaChart.lineTime.attr("style", 'opacity: 1;')
+  stackedAreaChart.textTime.attr("style", 'opacity: 1;')
 
 })
-
-
-
--- js/map.js
+++ js/map.js
@@ -1,166 +1,179 @@
-MapPlot = function(_parentElement, _dataGeo, _dataDrugs, _colorScale){
-    this.parentElement = _parentElement;
-    this.dataGeo = _dataGeo;
-    this.dataDrugs = _dataDrugs;
-    this.colorScale = _colorScale;
-    this.dataDrugsFull = _dataDrugs;
+MapPlot = function(_parentElement, _dataGeo, _dataDrugs, _colorScale) {
+  this.parentElement = _parentElement;
+  this.dataGeo = _dataGeo;
+  this.dataDrugs = _dataDrugs;
+  this.colorScale = _colorScale;
+  this.dataDrugsFull = _dataDrugs;
 
-    this.initVis();
+  this.initVis();
 };
 
 
-MapPlot.prototype.initVis = function(){
-    let vis = this;
+MapPlot.prototype.initVis = function() {
+  let vis = this;
 
-    vis.margin = { top: 40, right: 60, bottom: 60, left: 60 };
+  vis.margin = {
+    top: 40,
+    right: 60,
+    bottom: 60,
+    left: 60
+  };
 
-    vis.scale = (110000/ 953) * ($('#map').width());
+  vis.scale = (110000 / 953) * ($('#map').width());
 
-    vis.width = $('#map').width() - vis.margin.left - vis.margin.right,
-        vis.height = $('#map').width() / 2 - vis.margin.top - vis.margin.bottom;
+  vis.width = $('#map').width() - vis.margin.left - vis.margin.right,
+    vis.height = $('#map').width() / 2 - vis.margin.top - vis.margin.bottom;
 
-    console.log($('#map').width())
-    console.log(vis.scale)
-    // SVG drawing area
-    vis.svg = d3.select("#" + vis.parentElement).append("svg")
-        .attr("width", vis.width + vis.margin.left + vis.margin.right)
-        .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + vis.margin.left  + "," + vis.margin.top + ")");
+  console.log($('#map').width())
+  console.log(vis.scale)
+  // SVG drawing area
+  vis.svg = d3.select("#" + vis.parentElement).append("svg")
+    .attr("width", vis.width + vis.margin.left + vis.margin.right)
+    .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
 
-    vis.projection = d3.geoMercator()
-        .center([-71.068797,42.342923])
-        .translate([vis.width / 2, vis.height / 3])
-        .scale(vis.scale);
+  vis.projection = d3.geoMercator()
+    .center([-71.068797, 42.342923])
+    .translate([vis.width / 2, vis.height / 3])
+    .scale(vis.scale);
 
-    vis.path = d3.geoPath()
-        .projection(vis.projection);
+  vis.path = d3.geoPath()
+    .projection(vis.projection);
 
-    vis.legendToolDiv = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
+  vis.legendToolDiv = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-    vis.wrangleData();
+  vis.wrangleData();
 };
 
 
-MapPlot.prototype.wrangleData = function(){
-    let vis = this;
+MapPlot.prototype.wrangleData = function() {
+  let vis = this;
 
-    vis.updateVis();
+  vis.updateVis();
 };
 
 MapPlot.prototype.updateVis = function() {
-    var vis = this;
+  var vis = this;
 
-    var districts = vis.dataGeo.features;
+  var districts = vis.dataGeo.features;
 
-    // Render the U.S. by using the path generator
-    vis.svg.selectAll("path")
-        .data(districts)
-        .enter().append("path")
-        .attr("d", vis.path);
+  // Render the U.S. by using the path generator
+  vis.svg.selectAll("path")
+    .data(districts)
+    .enter().append("path")
+    .attr("d", vis.path);
 
-    var dots = vis.svg.selectAll('.crimeDot')
-        .data(vis.dataDrugs, function(d) {return d.INCIDENT_NUMBER + d.OFFENSE_CODE});
+  var dots = vis.svg.selectAll('.crimeDot')
+    .data(vis.dataDrugs, function(d) {
+      return d.INCIDENT_NUMBER + d.OFFENSE_CODE
+    });
 
-    var dotsEnter = dots
-        .enter()
+  var dotsEnter = dots
+    .enter()
 
-    dotsEnter
-        .append("circle")
-        .attr('class', 'crimeDot')
-        .attr("cx", function (d) { return vis.projection([d.Long, d.Lat])[0]; })
-        .attr("cy", function (d) { return vis.projection([d.Long, d.Lat])[1]; })
-        .attr('r', '2px')
-        .attr('fill', function (d) {
-            return vis.colorScale(d.OFFENSE_DESCRIPTION)
-        });
+  dotsEnter
+    .append("circle")
+    .attr('class', 'crimeDot')
+    .attr("cx", function(d) {
+      return vis.projection([d.Long, d.Lat])[0];
+    })
+    .attr("cy", function(d) {
+      return vis.projection([d.Long, d.Lat])[1];
+    })
+    .attr('r', '2px')
+    .attr('fill', function(d) {
+      return vis.colorScale(d.OFFENSE_DESCRIPTION)
+    });
 
-    dots.exit().remove();
+  dots.exit().remove();
 
 
-    var legendBoxWidth = 20;
-    var buffer = 10;
+  var legendBoxWidth = 20;
+  var buffer = 10;
 
-    vis.svg.selectAll('rect')
-        .data(vis.colorScale.domain())
-        .enter()
-        .append('rect')
-        .attr('width', legendBoxWidth)
-        .attr('height', legendBoxWidth)
-        .attr('fill', function (d) {
-            return vis.colorScale(d)
-        })
-        .attr('x', 10)
-        .attr('y', function(d, index) { return (legendBoxWidth + buffer) * index })
-        .on("mouseover", function(d) {
-            vis.legendToolDiv.transition()
-                .duration(200)
-                .style("opacity", .9);
-            vis.legendToolDiv.html(getDescriptions(d))
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function(d) {
-            vis.legendToolDiv.transition()
-                .duration(500)
-                .style("opacity", 0);
-        })
+  vis.svg.selectAll('rect')
+    .data(vis.colorScale.domain())
+    .enter()
+    .append('rect')
+    .attr('width', legendBoxWidth)
+    .attr('height', legendBoxWidth)
+    .attr('fill', function(d) {
+      return vis.colorScale(d)
+    })
+    .attr('x', 10)
+    .attr('y', function(d, index) {
+      return (legendBoxWidth + buffer) * index
+    })
+    .on("mouseover", function(d) {
+      vis.legendToolDiv.transition()
+        .duration(200)
+        .style("opacity", .9);
+      vis.legendToolDiv.html(getDescriptions(d))
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+    })
+    .on("mouseout", function(d) {
+      vis.legendToolDiv.transition()
+        .duration(500)
+        .style("opacity", 0);
+    })
 
-    vis.svg.selectAll('.legendLabel')
-        .data(vis.colorScale.domain())
-        .enter()
-        .append('text')
-        .text(function (d) {
-            return toTitleCase(d)
-        })
-        .attr('x', 35)
-        .attr('y', function(d, index) { return 15 + (legendBoxWidth + buffer) * index })
-        .attr('class', 'legendLabel')
-        .on('click', function (d) {
-            filter(d, vis)
-        })
-        .on("mouseover", function(d) {
-            vis.legendToolDiv.transition()
-                .duration(200)
-                .style("opacity", .9);
-            vis.legendToolDiv.html(getDescriptions(d))
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function(d) {
-            vis.legendToolDiv.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
+  vis.svg.selectAll('.legendLabel')
+    .data(vis.colorScale.domain())
+    .enter()
+    .append('text')
+    .text(function(d) {
+      return toTitleCase(d)
+    })
+    .attr('x', 35)
+    .attr('y', function(d, index) {
+      return 15 + (legendBoxWidth + buffer) * index
+    })
+    .attr('class', 'legendLabel')
+    .on('click', function(d) {
+      filter(d, vis)
+    })
+    .on("mouseover", function(d) {
+      vis.legendToolDiv.transition()
+        .duration(200)
+        .style("opacity", .9);
+      vis.legendToolDiv.html(getDescriptions(d))
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+    })
+    .on("mouseout", function(d) {
+      vis.legendToolDiv.transition()
+        .duration(500)
+        .style("opacity", 0);
+    });
 
-    vis.svg.append('text')
-        .attr('class', 'legendLabel')
-        .text('Hover for description. Click to filter.')
-        .attr('x', -5)
-        .attr('y', -5)
-        .attr('style', 'font-weight: bold');
+  vis.svg.append('text')
+    .attr('class', 'legendLabel')
+    .text('Hover for description. Click to filter.')
+    .attr('x', -5)
+    .attr('y', -5)
+    .attr('style', 'font-weight: bold');
 
 };
 
 // https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
 function toTitleCase(str) {
-    return str.replace(
-        /\w\S*/g,
-        function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-    );
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
 }
 
 function filter(crimeType, vis) {
-    vis.dataDrugs = vis.dataDrugsFull.filter(function (d) {
-        return (d.OFFENSE_DESCRIPTION == crimeType)
-    });
+  vis.dataDrugs = vis.dataDrugsFull.filter(function(d) {
+    return (d.OFFENSE_DESCRIPTION == crimeType)
+  });
 
-    vis.wrangleData()
+  vis.wrangleData()
 }
-
-
