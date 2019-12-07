4a27310985933b221267f2cf3a94433d7bd465cc
wfinigan <wfinigan64@gmail.com>
2019-12-07 13:25:59 -0500

    Fix scrolling.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 /* main JS file */
 
-// set up animations
-new WOW().init();
-
-var myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
-
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-}
 
 // settings for navigation
 var myFullpage = new fullpage('#fullpage', {
